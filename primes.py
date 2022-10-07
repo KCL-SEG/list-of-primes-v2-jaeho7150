43200b69a9666fdd66b836c2b17d0d9ec9f77f88
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-07 20:35:38 +0100

    put exception block out of while loop

--- primes.py
+++ primes.py
@@ -4,11 +4,10 @@
 def primes(number_of_primes):
     list = []
     current_num = 1
+    if number_of_primes < 1:
+        raise ValueError("Input number should be greater than 0")
+
     while len(list) < number_of_primes:
-
-        if number_of_primes < 1:
-            raise ValueError("Input number should be greater than 0")
-
         if current_num > 1:
             for i in range(2,current_num):
                 if (current_num % i) == 0:
